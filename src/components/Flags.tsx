4bff737d2275180e5f8b54789f90506f1676e916
sih3rron <siherron+github@gmail.com>
2021-08-15 09:31:22 +0100

    updated component to include dropdown actions. Need to fix CORS on fetch requests

--- src/components/Flags.tsx
+++ src/components/Flags.tsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react';
-import { EntityListItem } from '@contentful/forma-36-react-components';
+import { EntityListItem, DropdownList, DropdownListItem } from '@contentful/forma-36-react-components';
+import Patch from '../functions/Patch';
 const ldTag = "Contentful";
 const environment = "production";
 const projKey = "default";
@@ -48,6 +49,7 @@ export default function Flags(){
                     key={ i }  
                     title={ flag[1].name }
                     description={ flag[1].description }  
+                    dropdownListElements={<DropdownList><DropdownListItem onClick={()=>{Patch(flag[1].name, flag[1].environments.production.on)}}>{flag[1].environments.production.on === true ? "Deactivate" : "Activate"}</DropdownListItem></DropdownList>}
                     withThumbnail={ false }
                     thumbnailUrl="https://prismic-io.s3.amazonaws.com/launchdarkly/29b87739-0fa9-489a-bb0f-5aa825a10509_Feature_Flags_Icon.svg"
                     status={ flag[1].environments.production.on === true ? "published" : "draft" }
--- src/functions/Patch.ts
+++ src/functions/Patch.ts
@@ -1,10 +1,8 @@
 const projKey = "default";
 const environment = "production";
-const flagName = "";
 const apiToken = "api-bfb311e8-b47c-4539-980d-f28b92ac8612";
-const uri = `https://app.launchdarkly.com/api/v2/flags/${projKey}/${flagName}`;
 
-let patchConfig = {
+let patchConfigOn = {
 	"method": "Patch",
 	"headers": {
 		"Content-Type": "application/json",
@@ -17,10 +15,23 @@ let patchConfig = {
       })
 }
 
+let patchConfigOff = {
+	"method": "Patch",
+	"headers": {
+		"Content-Type": "application/json",
+		"authorization": apiToken,
+		"LD-API-Version": "beta"
+	},
+    "body": JSON.stringify({
+        "environmentKey": environment,
+        "instructions": [{"kind": "turnFlagOff"}]
+      })
+}
+
 // eslint-disable-next-line
-const FlagPatch = async ()=> {
+const FlagPatch = async (flagName: string, currentState: boolean)=> {
     try {
-        const response = await fetch(uri, patchConfig);
+        const response = await fetch(`https://app.launchdarkly.com/api/v2/flags/${projKey}/${flagName}`, currentState ? patchConfigOn : patchConfigOff);
         const json = await response.json();
         json.status === 200 ? console.log("\x1b[32m","PATCH: Fine.") : console.log("\x1b[31m",`PATCH Fail: We have a ${json.status}`);
     }
